1b52c604d290b33aee3f8f23e3159ed20ec66bdb
Mahyar Emad <mtmralone@gmail.com>
2023-03-30 11:02:47 +0330

    Animated Watching Server And FiveM

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import discord
-from discord import app_commands
 from discord.ext import commands, tasks
+# from discord import app_commands
 
-import json, requests
+import json, requests, asyncio
 
 
 with open("config.json") as f:
@@ -14,21 +14,25 @@ bot = commands.Bot(command_prefix="infinity!", intents=discord.Intents.all())
 @bot.event
 async def on_ready():
     print("Bot is Up and Ready!")
-    change_status.start()
-
-    # try:
-    #     synced = await bot.tree.sync()
-    #     print(f"Synced {len(synced)} command(s)")
-    # except Exception as e:
-    #     print(f"{e}")
+    fivem_status.start()
+    await asyncio.sleep(15)
+    server_status.start()
 
 @tasks.loop(seconds=30)
-async def change_status():
+async def server_status():
+    await bot.wait_until_ready()
+
+    guild = bot.get_guild(863866118677987349).member_count
+    text = f"{guild} Members!"
+    activity = discord.Activity(type=discord.ActivityType.watching, name=text)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
+@tasks.loop(seconds=30)
+async def fivem_status():
     await bot.wait_until_ready()
 
     req = requests.get(config["api"])
     response = json.loads(req.text)["player"]
-    # guild = bot.get_guild(863866118677987349)
     if response != -1:
         text = f"🟢 {response} Players!"
     else:
