f185c5fb5a47e116bc7ab4a462790229c5930ac7
Mahyar Emad <mtmralone@gmail.com>
2023-07-16 13:20:24 +0330

    Fixed groups

--- group.py
+++ group.py
@@ -9,21 +9,21 @@ with open("config.json") as f:
     config = json.load(f)
 
 choices = []
-for i in range(len(config["factions"])):
-    choices.append(app_commands.Choice(name=config["factions"][i]["name"], value=i))
+for i in range(len(config["groups"])):
+    choices.append(app_commands.Choice(name=config["groups"][i]["name"], value=i))
 
-class Faction(commands.Cog):
+class Group(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         super().__init__()  # this is now required in this context.
 
     @app_commands.command(name="give", description="Give role to a user")
-    @app_commands.describe(user="Mention user", faction="Select your faction", role="Select role to give")
-    @app_commands.choices(faction=choices)
+    @app_commands.describe(user="Mention user", group="Select your group", role="Select role to give")
+    @app_commands.choices(group=choices)
     async def give(self, interaction: discord.Interaction, user: discord.Member, 
-                   faction: app_commands.Choice[int], role: discord.Role):
-        moderator = interaction.guild.get_role(config["factions"][faction.value]["moderator"])
-        roles = [interaction.guild.get_role(i) for i in config["factions"][faction.value]["roles"]]
+                   group: app_commands.Choice[int], role: discord.Role):
+        moderator = interaction.guild.get_role(config["groups"][group.value]["moderator"])
+        roles = [interaction.guild.get_role(i) for i in config["groups"][group.value]["roles"]]
         if not (moderator in interaction.user.roles) or not (role in roles) or role in user.roles:
             embed = discord.Embed(title=config["embed_header"], description="You can't give role.")
             await interaction.response.send_message(embed=embed, ephemeral=True)
@@ -35,12 +35,12 @@ class Faction(commands.Cog):
         await interaction.response.send_message(embed=embed)
     
     @app_commands.command(name="take", description="Take role to a user")
-    @app_commands.describe(user="Mention user", faction="Select your faction", role="Select role to take")
-    @app_commands.choices(faction=choices)
+    @app_commands.describe(user="Mention user", group="Select your group", role="Select role to take")
+    @app_commands.choices(group=choices)
     async def take(self, interaction: discord.Interaction, user: discord.Member, 
-                   faction: app_commands.Choice[int], role: discord.Role):
-        moderator = interaction.guild.get_role(config["factions"][faction.value]["moderator"])
-        roles = [interaction.guild.get_role(i) for i in config["factions"][faction.value]["roles"]]
+                   group: app_commands.Choice[int], role: discord.Role):
+        moderator = interaction.guild.get_role(config["groups"][group.value]["moderator"])
+        roles = [interaction.guild.get_role(i) for i in config["groups"][group.value]["roles"]]
         if not (moderator in interaction.user.roles) or not (role in roles) or not (role in user.roles):
             embed = discord.Embed(title=config["embed_header"], description="You can't take role.")
             await interaction.response.send_message(embed=embed, ephemeral=True)
@@ -54,4 +54,4 @@ class Faction(commands.Cog):
         
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(Faction(bot))
+    await bot.add_cog(Group(bot))
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands, tasks
 from discord import app_commands
 
-import json, requests, asyncio, faction
+import json, group
 
 
 with open("config.json") as f:
@@ -11,16 +11,16 @@ with open("config.json") as f:
 
 bot = commands.Bot(command_prefix="infinity!", intents=discord.Intents.all())
 
+
 @bot.event
 async def setup_hook():
     print("Cogs loaded!")
-    await faction.setup(bot)
+    await group.setup(bot)
+
 
 @bot.event
 async def on_ready():
     print("Bot is Up and Ready!")
-    fivem_status.start()
-    await asyncio.sleep(15)
     server_status.start()
     try:
         synced = await bot.tree.sync()
@@ -28,25 +28,13 @@ async def on_ready():
     except Exception as e:
         print(f"{e}")
 
+
 @tasks.loop(seconds=30)
 async def server_status():
     await bot.wait_until_ready()
 
-    guild = bot.get_guild(1085134260215758910).member_count
+    guild = bot.get_guild(config["guild"]).member_count
     text = f"{guild} Members!"
-    activity = discord.Activity(type=discord.ActivityType.watching, name=text)
-    await bot.change_presence(status=discord.Status.online, activity=activity)
-
-@tasks.loop(seconds=30)
-async def fivem_status():
-    await bot.wait_until_ready()
-
-    req = requests.get(config["api"])
-    response = json.loads(req.text)["player"]
-    if response != -1:
-        text = f"🟢 {response} Players!"
-    else:
-        text = f"🔴 Offline!"
     activity = discord.Activity(type=discord.ActivityType.watching, name=text)
     await bot.change_presence(status=discord.Status.online, activity=activity)
 
